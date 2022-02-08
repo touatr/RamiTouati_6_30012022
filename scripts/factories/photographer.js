6fa57cf5f4f22dc877b03377cb7e8509201496a9
touatr <touati.ram@gmail.com>
2022-02-08 19:13:46 +0100

    Ajout du lien sur l'image photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,9 +4,12 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );//créer un élement article 
+        const article = document.createElement( 'article' );//créer un élement article
+        const photographerLinkPage = document.createElement('a');
+        photographerLinkPage.setAttribute("href", "photographer.html");
         const img = document.createElement( 'img' );//Créer un élement img
         img.setAttribute("src", picture)//Affecter une image et une src
+        photographerLinkPage.appendChild(img);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         const h3 = document.createElement('h3');
@@ -15,7 +18,8 @@ function photographerFactory(data) {
         p.textContent = tagline;
         const h5 = document.createElement('h5');
         h5.textContent = price + '€/jour';
-        article.appendChild(img);//Ajouter l'élement img
+        //article.appendChild(img);//Ajouter l'élement img
+        article.appendChild(photographerLinkPage);
         article.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(p);
